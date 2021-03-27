adfd81a71a6ea3e1cddca2c95bd940ed633ba3b5
yoved <sean.m.devoy@gmail.com>
2021-03-27 19:24:27 -0400

    new test cases

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,9 +2,9 @@ const request = require('supertest')
 const app = require('../src/app')
 
 test('Should signup a new user', async()=>{
-    await await request(app).post('/users').send({
+    await request(app).post('/users').send({
         name: 'Sean',
-        email:'Sean.m.d@gmail.com',
+        email:'Sean.m.dev@gmail.com',
         password: 'Hashme123'
     }).expect(201)//expect the success response you define in router
 })
